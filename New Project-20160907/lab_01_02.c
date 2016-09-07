cb41bd1a5e5eae42dc427d5a71198c0631863f6f
lisamurchik <lisamurchik@icloud.com>
2016-09-07 15:51:50 +0400

    Created by Coding Ground Client

---
+++ New Project-20160907/lab_01_02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main () {
+    char c1=110,
+         c2='t';
+    printf("c1 = %d\n", c1);
+    printf("c1=%c\n", c1);
+    printf( "c2=%d\n", c2);
+    const int coef=2016;
+    double a = 1005.23125478965,
+           b=5.347;
+    printf("%7d \n", coef);
+    printf("%7.1lf \n", b); 
+    printf ("%.7f \n", a);
+    printf ("%.3f \n", b);
+    printf ("%5.7f \n", a);
+    printf ("%7.1f \n", b);
+    printf("%7d \n", coef);
+}
+    
